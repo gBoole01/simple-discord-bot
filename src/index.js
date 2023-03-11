01f780824467f4c7557feb33fa09916316fa9513
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2023-03-11 21:45:42 +0100

    featt: register slash commands

--- src/index.js
+++ src/index.js
@@ -1,13 +1,57 @@
-const { Client, GatewayIntentBits, Events } = require('discord.js');
-const dotenv = require('dotenv');
+const fs = require("fs");
+const path = require("path");
+const { Client, Collection, GatewayIntentBits, Events } = require("discord.js");
+const dotenv = require("dotenv");
 dotenv.config();
-const token = process.env.DISCORD_TOKEN;
 
-const client = new Client({intents: [GatewayIntentBits.Guilds] })
-
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.once(Events.ClientReady, (c) => {
-    console.log(`Logged in as ${c.user.tag}!`);
-})
+  console.log(`Logged in as ${c.user.tag}!`);
+});
 
-client.login(token);
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const command = require(path.join(commandsPath, file));
+  if ("data" in command && "execute" in command) {
+    client.commands.set(command.data.name, command);
+  } else {
+    console.error(`Failed to load command ${file}`);
+  }
+}
+
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+  const command = interaction.client.commands.get(interaction.commandName);
+
+  if (!command) {
+    console.error("Command not found");
+    return;
+  }
+
+  try {
+    await command.execute(interaction);
+  } catch (e) {
+    console.error(e);
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    } else {
+      await interaction.reply({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    }
+  }
+});
+
+const { DISCORD_TOKEN } = process.en;
+client.login(DISCORD_TOKEN);
